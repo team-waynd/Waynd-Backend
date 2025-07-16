dcec7d6306a30edc9d123e5ee4f70415f9ead27c
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-07-16 15:11:24 +0900

    [Feat] #12 - 유저 정보 수정 api 구현

--- src/modules/auth/strategies/jwt-access.strategy.ts
+++ src/modules/auth/strategies/jwt-access.strategy.ts
@@ -1,8 +1,10 @@
 import { ExtractJwt, Strategy as JwtStrategy } from 'passport-jwt';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 
 import { ConfigService } from '@nestjs/config';
-import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
+import { User } from 'src/modules/user/user.entity';
+import { UserService } from 'src/modules/user/user.service';
 
 interface JwtPayload {
   sub: string;
@@ -11,14 +13,21 @@ interface JwtPayload {
 
 @Injectable()
 export class JwtAccessStrategy extends PassportStrategy(JwtStrategy, 'jwt') {
-  constructor(private readonly config: ConfigService) {
+  constructor(
+    private readonly config: ConfigService,
+    private readonly userService: UserService,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: config.get<string>('JWT_ACCESS_SECRET') ?? '',
     });
   }
 
-  validate(payload: JwtPayload): JwtPayload {
-    return payload;
+  async validate(payload: JwtPayload): Promise<User> {
+    const user = await this.userService.findById(payload.sub);
+    if (!user) {
+      throw new UnauthorizedException('User not found');
+    }
+    return user;
   }
 }
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { JwtAuthGuard } from '../auth/guards/auth.guard';
 import { User } from './user.entity';
@@ -17,9 +17,29 @@ export class UserController {
     const userId = req.user.id;
     const userInfo = await this.userService.findById(userId);
     return {
+      userId: userInfo?.id,
       name: userInfo?.name,
       email: userInfo?.email,
       profileImg: userInfo?.profileImage,
     };
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('me')
+  editMyProfile(
+    @Req() req: { user: User },
+    @Body() body: { name: string; email: string },
+  ) {
+    const userId = req.user.id;
+
+    if (!userId) {
+      throw new Error('User ID not found in request');
+    }
+
+    return this.userService.updateProfile({
+      userId,
+      name: body.name,
+      email: body.email,
+    });
   }
 }
