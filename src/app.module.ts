ea71d328f973c920b7e31b0602576d45870e10ae
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-07-31 22:08:25 +0900

    [Fix] #19 - 불필요한 파일 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,6 @@ import { AuthModule } from './modules/auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { FestivalModule } from './modules/festival/festival.module';
 import { Module } from '@nestjs/common';
-// 앞으로 여기에 실제 기능 모듈들을 import
-// import { UserModule } from './modules/user/user.module';
-// import { AuthModule } from './modules/auth/auth.module';
 import { PlaceModule } from './modules/place/place.module';
 import { PostModule } from './modules/post/post.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
