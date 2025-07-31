bb1125bdfb87619bf86608d4d3393cf110e1e8ec
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-07-31 16:31:43 +0900

    feat: main.ts에 Swagger 설정
    #19

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
-import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Waynd-Backend')
+    .setDescription('Waynd의 API Swagger입니다.')
+    .setVersion('1.0')
+    // .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
 
   app.useGlobalPipes(
     new ValidationPipe({
