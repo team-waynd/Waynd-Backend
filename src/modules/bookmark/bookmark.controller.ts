0a60bbf5e9fd914e402a842d011b7295b4f05120
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-08-03 16:15:39 +0900

    [Feat] 축제, 장소, 음식점 찜 조회 및 찜 토글 기능 개발
    #20 #21 #22 #23

--- src/modules/bookmark/bookmark.controller.ts
+++ src/modules/bookmark/bookmark.controller.ts
@@ -1,7 +1,38 @@
+import { Controller, Get, Post } from '@nestjs/common';
+
 import { BookmarkService } from './bookmark.service';
-import { Controller } from '@nestjs/common';
 
 @Controller('bookmark')
 export class BookmarkController {
   constructor(private readonly bookmarkService: BookmarkService) {}
+
+  @Get('festival')
+  getBookmarkFestivals(userId: string) {
+    return this.bookmarkService.getBookmarkFestivals(userId);
+  }
+
+  @Post('festival')
+  toggleBookmarkFestival(userId: string, festivalId: string) {
+    return this.bookmarkService.toggleBookmarkFestival(userId, festivalId);
+  }
+
+  @Get('spot')
+  getBookmarkSpots(userId: string) {
+    return this.bookmarkService.getBookmarkSpots(userId);
+  }
+
+  @Post('spot')
+  toggleBookmarkSpot(userId: string, spotId: string) {
+    return this.bookmarkService.toggleBookmarkSpot(userId, spotId);
+  }
+
+  @Get('food')
+  getBookmarkRestaurants(userId: string) {
+    return this.bookmarkService.getBookmarkRestaurants(userId);
+  }
+
+  @Post('food')
+  toggleBookmarkRestaurant(userId: string, restaurantId: string) {
+    return this.bookmarkService.toggleBookmarkRestaurant(userId, restaurantId);
+  }
 }
--- src/modules/bookmark/bookmark.entity.ts
+++ src/modules/bookmark/bookmark.entity.ts
@@ -4,6 +4,7 @@ import {
   JoinColumn,
   ManyToOne,
   PrimaryGeneratedColumn,
+  RelationId,
 } from 'typeorm';
 
 import { User } from '../user/user.entity';
@@ -12,11 +13,17 @@ import { User } from '../user/user.entity';
 export class Bookmark {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
   @Column()
   type: 'spot' | 'food' | 'festival';
+
   @Column()
   ref: string;
+
   @ManyToOne(() => User, (user) => user.bookmarks, { onDelete: 'CASCADE' })
   @JoinColumn({ name: 'userId' })
   user: User;
+
+  @RelationId((bookmark: Bookmark) => bookmark.user)
+  userId: string;
 }
